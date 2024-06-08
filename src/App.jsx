29008f540bbd2a6eadcbf1594d97659edda7cb58
Nagendra Anala <Nagendraanala@gmail.com>
2024-06-08 19:33:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Route , Routes , Navigate } from 'react-router-dom';
+import Signup from './components/Signup/index';
+import Login from './components/Login/index';
+import './App.css'
+function App(props) {
+
+  const appType = props.appType; 
+
+  const user = localStorage.getItem('token');
+  return (
+    <Routes>
+      {user && <Route path='/' exact element = {props.children} />}
+      <Route path='/signup' exact element = {<Signup appType = {appType}/>} />
+      <Route path='/login' exact element = {<Login appType = {appType}/>} />
+      <Route path='/' exact element = {<Navigate replace to='/login'/>} />
+    </Routes>
+  )
+}
+
+export default App
