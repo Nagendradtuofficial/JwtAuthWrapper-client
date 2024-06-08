3cb539c295260ce32b0b5266f9256970a70c2b78
Nagendra Anala <Nagendraanala@gmail.com>
2024-06-09 02:12:21 +0530

    logout function

--- src/JWTAuthWrapper.jsx
+++ src/JWTAuthWrapper.jsx
@@ -4,16 +4,17 @@ import Signup from './components/Signup/index';
 import Login from './components/Login/index';
 import {BrowserRouter} from 'react-router-dom'
 import './JWTAuthWrapper.css'
+
+export const logout_user = () => {
+  localStorage.removeItem('token');
+  window.location.reload();
+}
+
 function JWTAuthWrapper(props) {
 
   const appType = props.appType; 
 
   const user = localStorage.getItem('token');
-
-  const logout_user = () => {
-    localStorage.removeItem('token');
-    window.location.reload();
-  }
 
   return (
     <BrowserRouter>
