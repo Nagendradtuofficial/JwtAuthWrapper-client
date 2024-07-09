ca0331a2b20ab234a9b2343639ba237e7cc782fa
Nagendra Anala <Nagendraanala@gmail.com>
2024-07-10 00:00:24 +0530

    Responsive for mweb

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import styles from './login_styles.module.css' ;
+// import styles from './login_styles.module.css' ;
+import styles from '../styles.module.css' ;
 import { useState } from 'react';
 import axios from 'axios' ;
 import { Loader } from '../Loader';
